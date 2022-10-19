0ae89b833a9b7545d5901069056baeb3d44d4bd1
dataweb <bert@data-web.be>
2022-10-19 12:07:46 +0200

    Bootstrap CSS, custom CSS/JS.

---
+++ custom/custom.js
@@ -0,0 +1,31 @@
+(function ($) {
+  Drupal.dw_drp_base_custom = Drupal.dw_drp_base_custom || {};
+
+  Drupal.behaviors.dw_drp_base_custom = {
+    attach: function (context) {
+      addUiButton();
+    }
+  };
+
+  $(window).on({
+    'dialog:aftercreate': function afterCreateDialog() {
+      $('.ui-dialog-titlebar-close').each(function() {
+        if (!$(this).hasClass('ui-button')) {
+          $(this).addClass('ui-button ui-corner-all ui-widget ui-button-icon-only ui-dialog-titlebar-close');
+          $(this).html('<span class="ui-button-icon ui-icon ui-icon-closethick"></span><span class="ui-button-icon-space"> </span>Close');
+        }
+      });
+      addUiButton();
+    }
+  });
+
+  function addUiButton() {
+    $('form .button, .ui-dialog-buttonset .button').each(function() {
+      if (!$(this).hasClass('ui-button') &&
+          !$(this).hasClass('media-library-item__remove')) {
+        $(this).addClass('ui-button');
+      }
+    });
+  }
+
+})(jQuery);
